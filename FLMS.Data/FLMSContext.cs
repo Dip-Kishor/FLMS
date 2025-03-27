6d6971d3456314eb79e5aceccd9f18a12ef82e31
Dip-Kishor <dipkishor9910@gmail.com>
2025-03-27 07:39:31 +0545

    Added Table screen for user with SP

--- FLMS.Data/FLMSContext.cs
+++ FLMS.Data/FLMSContext.cs
@@ -1,13 +1,12 @@
-﻿using FLMS.Models;
+﻿using FLMS.Data.SP_Result;
+using FLMS.Models;
 using FLMS.Models.FixturesAndResults;
 using FLMS.Models.PlayerRegistration;
 using FLMS.Models.User;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
 
 namespace FLMS.Data
 {
@@ -22,5 +21,55 @@ namespace FLMS.Data
         public DbSet<ERegisteredPlayers> RegisteredPlayers { get; set; }
         public DbSet<ESeason> Seasons { get; set; }
         public DbSet<EFixturesAndResults> FixturesAndResults { get; set; }
+
+        //public async Task<List<Table_SP_Results>> GetTableReportAsync(int seasonId)
+        //{
+        //    var results = new List<Table_SP_Results>();
+
+        //    using (var command = this.Database.GetDbConnection().CreateCommand())
+        //    {
+        //        command.CommandText = "SP_Get_TableReport";
+        //        command.CommandType = CommandType.StoredProcedure;
+
+        //        // Add parameter for stored procedure
+        //        var seasonParam = new SqlParameter("@SeasonId", SqlDbType.Int) { Value = seasonId };
+        //        command.Parameters.Add(seasonParam);
+
+        //        await this.Database.OpenConnectionAsync();
+
+        //        using (var reader = await command.ExecuteReaderAsync())
+        //        {
+        //            while (await reader.ReadAsync())
+        //            {
+        //                results.Add(new Table_SP_Results
+        //                {
+        //                    Group = (GroupType)reader["Group"],
+        //                    Rank = Convert.ToInt32(reader.GetInt64(reader.GetOrdinal("Rank"))),
+        //                    PlayerId = reader.GetInt32(reader.GetOrdinal("PlayerId")),
+        //                    PlayerName = reader.GetString(reader.GetOrdinal("PlayerName")),
+        //                    MatchesPlayed = reader.GetInt32(reader.GetOrdinal("MatchesPlayed")),
+        //                    Wins = reader.GetInt32(reader.GetOrdinal("Wins")),
+        //                    Draws = reader.GetInt32(reader.GetOrdinal("Draws")),
+        //                    Losses = reader.GetInt32(reader.GetOrdinal("Losses")),
+        //                    GF = reader.GetInt32(reader.GetOrdinal("GF")),
+        //                    GA = reader.GetInt32(reader.GetOrdinal("GA")),
+        //                    GD = reader.GetInt32(reader.GetOrdinal("GD")),
+        //                    Points = reader.GetInt32(reader.GetOrdinal("Points")),
+        //                });
+        //            }
+        //        }
+        //    }
+
+        //    return results;
+        //}
+        public async Task<List<Table_SP_Results>> GetTableReportAsync(int seasonId)
+        {
+            var seasonIdParam = new SqlParameter("@SeasonId", seasonId);
+            StoredPocedureExtension spEx = new StoredPocedureExtension(this);
+            return await spEx.ExecuteStoredProcedureAsync<Table_SP_Results>("SP_Get_TableReport",
+             seasonIdParam
+                );
+        }
+
     }
 }
---
+++ FLMS.Data/SP_Result/Table_SP_Results.cs
@@ -0,0 +1,27 @@
+﻿using FLMS.Models.FixturesAndResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLMS.Data.SP_Result
+{
+    public class Table_SP_Results
+    {
+        public int Group { get; set; }
+        public long Rank { get; set; }
+        public int PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public string ImageUrl { get; set; }
+        public int MatchesPlayed { get; set; }
+        public int Wins { get; set; }
+        public int Draws { get; set; }
+        public int Losses { get; set; }
+        public int GF {  get; set; }
+        public int GD {  get; set; }
+        public int GA {  get; set; }
+        public int Points { get; set; }
+    }
+
+}
